3d0d51a705bcf569c967a5dcdd85afe0234410fe
caio-lucena <caio.lucena10@hotmail.com>
2023-10-25 09:21:43 -0300

    continuação dos estudos de estruras de dados com Java

---
+++ src/lista/modelo/ListaEncadeada.java
@@ -0,0 +1,58 @@
+package lista.modelo;
+
+public class ListaEncadeada<T> {
+	
+	No<T> referenciaEntrada;
+	
+	public ListaEncadeada(){
+		this.referenciaEntrada = null;
+	}
+	
+	public boolean isEmpty() {
+		return this.referenciaEntrada == null ? true: false;
+	}
+	
+	public Integer size () {
+		Integer tamanhoLista = 0;
+		
+		No <T> referenciaAuxiliar = referenciaEntrada;
+		
+		while (true) {
+			
+			if(referenciaAuxiliar != null) {
+				tamanhoLista++;
+				
+				if(referenciaAuxiliar.getProximoNo() != null) {
+					referenciaAuxiliar = referenciaAuxiliar.getProximoNo();
+				}
+				else {
+					break;
+				}
+			}
+			else {
+				break;
+			}
+		}
+		
+		return tamanhoLista;
+	}
+	
+	public void add (T conteudo) {
+		No<T> novoNo = new No<>(conteudo);
+		
+		if(this.isEmpty()) {
+			referenciaEntrada = novoNo;
+			return;
+		}
+		
+		No<T> noAuxiliar = referenciaEntrada;
+		
+		for (int i = 0; i < this.size()-1; i++) {
+			noAuxiliar = noAuxiliar.getProximoNo();
+		}
+		
+		noAuxiliar.setProximoNo(novoNo);				
+	}
+	
+
+}
---
+++ src/lista/modelo/No.java
@@ -0,0 +1,56 @@
+package lista.modelo;
+
+public class No <T>{
+	
+	private T conteudo;
+	private No<T> proximoNo;
+	
+	public No() {
+		this.proximoNo = null;
+	}
+
+	public No(T conteudo) {
+		this.conteudo = conteudo;
+		this.proximoNo = null;
+	}
+
+	public No(T conteudo, No<T> proximoNo) {
+		this.conteudo = conteudo;
+		this.proximoNo = proximoNo;
+	}
+
+	public T getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(T conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public No<T> getProximoNo() {
+		return proximoNo;
+	}
+
+	public void setProximoNo(No <T> proximoNo) {
+		this.proximoNo = proximoNo;
+	}
+
+	@Override
+	public String toString() {
+		return "No [conteudo = " + conteudo + "]";
+	}
+	
+	public String toStringEncadeado() {
+		String mensagem =  "No [conteudo = " + conteudo + "]";
+		
+		if(conteudo != null) {
+			mensagem += "->" + proximoNo.toString();
+		}
+		else {
+			mensagem += "-> Null";
+		}
+		
+		return mensagem;
+	}
+
+}
