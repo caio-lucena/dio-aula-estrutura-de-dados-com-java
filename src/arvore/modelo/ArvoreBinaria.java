9fcca8bb8a45cf68a2af230a6e17240d3a8c6138
caio-lucena <caio.lucena10@hotmail.com>
2023-12-01 11:18:05 -0300

    Concluindo métodos da ArvoreBinaria antes de começar os testes

--- src/arvore/modelo/ArvoreBinaria.java
+++ src/arvore/modelo/ArvoreBinaria.java
@@ -70,5 +70,106 @@ public class ArvoreBinaria<T extends Comparable<T>> {
 			exibirPreOrdem(atual.getNoDireita());
 		}
 	}
+	
+	public void remover(T conteudo) {
+		try {
+			
+			NoBinario<T> noAtual = this.raiz;
+			NoBinario<T> noPai = null;
+			NoBinario<T> noFilho = null;
+			NoBinario<T> noTemp = null;
+			
+			while(noAtual != null && !noAtual.getConteudo().equals(conteudo)) {
+				noPai = noAtual;
+				
+				if(conteudo.compareTo(noAtual.getConteudo()) < 0) {
+					noAtual = noAtual.getNoEsquerda();
+				}
+				else {
+					noAtual = noAtual.getNoDireita();
+				}
+			}
+			
+			if(noAtual == null) {
+				System.out.println("Conteúdo vazio");
+			}
+			
+			if(noPai == null) {
+				
+				if(noAtual.getNoDireita() == null ) {
+					this.raiz = noAtual.getNoEsquerda();					
+				}
+				else if(noAtual.getNoEsquerda() == null) {
+					this.raiz = noAtual.getNoDireita();
+					
+				}else {
+					for(
+						noTemp = noAtual, noFilho = noAtual.getNoEsquerda();
+						noFilho.getNoDireita() != null;
+						noTemp = noFilho, noFilho = noFilho.getNoEsquerda()
+						)
+					{
+						if(noFilho != noAtual.getNoEsquerda()) {
+							noTemp.setNoDireita(noFilho.getNoEsquerda());
+							noFilho.setNoEsquerda(this.raiz.getNoEsquerda());
+						}
+					}
+					
+					noFilho.setNoDireita(this.raiz.getNoDireita());
+					this.raiz = noFilho;
+					
+				}			
+				
+			}
+			else if (noAtual.getNoDireita() == null) {
+				
+				if(noPai.getNoEsquerda() == noAtual) {
+					noPai.setNoEsquerda(noAtual.getNoEsquerda());
+				}
+				else {
+					noPai.setNoDireita(noAtual.getNoEsquerda());
+				}
+				
+			}
+			else if(noAtual.getNoEsquerda() == null) {
+				
+				if(noPai.getNoEsquerda() == noAtual) {
+					noPai.setNoEsquerda(noAtual.getNoDireita());
+				}
+				else {
+					noPai.setNoDireita(noAtual.getNoDireita());
+				}
+				
+			}
+			else {				
+				
+				for(
+					noTemp = noAtual, noFilho = noAtual.getNoEsquerda();
+					noFilho.getNoDireita() != null;
+					noTemp = noFilho, noFilho = noFilho.getNoDireita()
+					)				
+				{
+					if(noFilho != noAtual.getNoEsquerda()) {
+						noTemp.setNoDireita(noFilho.getNoEsquerda());
+						noFilho.setNoEsquerda(noAtual.getNoEsquerda());
+					}
+					
+					noFilho.setNoDireita(noAtual.getNoDireita());
+					
+					if(noPai.getNoEsquerda() == noAtual) {
+						noPai.setNoEsquerda(noFilho);
+					}
+					else {
+						noPai.setNoDireita(noFilho);
+					}
+					
+				}				
+			}
+			
+		}
+		catch(NullPointerException ex) {
+			System.out.println("Conteúdo vazio: " + ex.getMessage());
+		}
+	}
 
 }
